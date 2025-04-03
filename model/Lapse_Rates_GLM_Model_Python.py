5af2e7b6e7c5ea368ba181d73239856750a5d7d2
ajordonez <ajordonez179@gmail.com>
2025-04-03 11:14:56 -0400

    Changed Model to get rid of empty values

--- model/Lapse_Rates_GLM_Model_Python.py
+++ model/Lapse_Rates_GLM_Model_Python.py
@@ -13,15 +13,18 @@ url = "https://raw.githubusercontent.com/ajordonez/Lapse_Rate_GLM_Model/refs/hea
 # We will use pandas to turn this data into a dataframe
 df = pd.read_csv(url)
 
+
+
+# We need to clean the data of blanks and ND 
+
+df.replace("ND", np.nan, inplace=True)
+df_predictor = df.iloc[:,0:8]
+df_predictor = df_predictor.dropna()
+print(df_predictor)
+
+
 # Print head of data
 print("New Line\n")
-print(df.head())
-
-
-# We need to clean the data of blanks and ND
-
-print(df.columns)
-
-df = df.dropna()
+# print(df.head())
 
 
