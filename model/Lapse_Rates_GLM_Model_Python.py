f232d92886549e1f9a3b8e04c2bfafe62ec579d6
ajordonez <ajordonez179@gmail.com>
2025-04-07 14:17:44 -0400

    Added to the model an outline of how we will inplement the GLM function.

--- model/Lapse_Rates_GLM_Model_Python.py
+++ model/Lapse_Rates_GLM_Model_Python.py
@@ -5,7 +5,10 @@
 import math
 import numpy as np
 import pandas as pd
-import sklearn as sk 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
 
 # We will take the url in our github for the raw csv files
 url = "https://raw.githubusercontent.com/ajordonez/Lapse_Rate_GLM_Model/refs/heads/main/data/Uncleaned_Predictive_Analytics.csv"
@@ -23,4 +26,30 @@ df = df.dropna()
 print(df)
 
 
+'''
+# The following will show how we intend to use the data provided to make a GLM using either Poisson or Negative Binomial distribution
 
+# Preparing the data
+
+X = df[['predictor1', 'predictor2']]
+y = df['binary_outcome']
+# Adding a constant to the predictor variable set
+X = sm.add_constant(X)
+# Logistic Regression model
+model = sm.GLM(y, X, family=sm.families.NegativeBinomial()).fit()
+# or 
+# Poisson Regression model
+model = sm.GLM(y, X, family=sm.families.Poisson()).fit()
+
+# Model summary
+print(model.summary())
+
+
+# For testing if the model works try the below methods for a poisson model
+
+# Calculate deviance
+deviance = model.deviance
+print(f"Deviance: {deviance}")
+
+
+'''
