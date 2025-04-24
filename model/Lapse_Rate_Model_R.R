78cf591caf2d82f0152ebf0a6c56cf4a234ca8fb
ajordonez <ajordonez179@gmail.com>
2025-04-24 10:46:28 -0400

    Made changes to Model

--- model/Lapse_Rate_Model_R.R
+++ model/Lapse_Rate_Model_R.R
@@ -2,3 +2,11 @@
 
 # Collaborators: Simon Zheng, Tanna Henry, Alejandro Ordonez
 
+# If not installed: install.packages("tidyverse")
+library(tidyverse)
+
+raw_file = read.csv("https://raw.githubusercontent.com/ajordonez/Lapse_Rate_GLM_Model/refs/heads/main/data/Uncleaned_Predictive_Analytics.csv")
+
+lapse_data = data.frame(raw_file)
+
+lapse_data = lapse_data %>% filter("")
